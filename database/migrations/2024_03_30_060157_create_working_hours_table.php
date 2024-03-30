1e8a2c8c8146b47a5dc3e912b41fc7df27f81ad1
mukhlish32@ <mukhlish.syarif@gmail.com>
2024-03-30 13:47:06 +0700

    create: database migration and seeder

---
+++ database/migrations/2024_03_30_060157_create_working_hours_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('working_hours', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('working_hours');
+    }
+};
